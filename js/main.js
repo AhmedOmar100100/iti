5e978de4e276a222a4e5fd65dfb29124b5e80b74
ahmed omar <ahmedomar234_sd@fcis.bsu.edu.eg>
2021-08-26 12:51:04 +0200

    alter in index page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("ahmed");
+// owl carousel courses
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        950: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
