067b3f4b026814bd063a5d940a0ef7cf3a8199c4
ahmed omar <ahmedomar234_sd@fcis.bsu.edu.eg>
2021-08-31 12:48:46 +0200

    finish home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // owl carousel courses
-$('.owl-carousel').owlCarousel({
+$('.courses-carousel').owlCarousel({
     loop: true,
     margin: 10,
     responsive: {
@@ -9,10 +9,24 @@ $('.owl-carousel').owlCarousel({
         600: {
             items: 1
         },
-        950: {
+        1014: {
             items: 1
         },
-        1000: {
+        1015: {
+            items: 2
+        },
+        1230: {
+            items: 3
+        }
+    }
+})
+$('.testimonial-carousel').owlCarousel({
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        767: {
             items: 2
         }
     }
@@ -25,3 +39,49 @@ function toggle() {
     var watching = document.querySelector(".watching");
     watching.classList.toggle("active");
 }
+
+
+
+// count down until event time
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = new Date(Date.parse(new Date()) + 2 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
